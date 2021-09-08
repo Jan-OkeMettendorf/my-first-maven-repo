ef88d58b140819c3bfddc9f6543f3318a7cb5b09
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-08 17:07:41 +0200

    add code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public static void main(String[] args){
+
+        // variables
+
+        int number1 = 2;
+        double number2 = 101;
+        String word = "cyalaekg";
+
+        // print
+
+        System.out.println("number1: " + number1 + "; number2: " + number2 + "; word: " + word);
+        System.out.println("calculateAdd: " + calculateAdd(number1, number2));
+        System.out.println("calculateSub: " + calculateSub(number1, number2));
+        System.out.println("calculateMul: " + calculateMul(number1, number2));
+        System.out.println("calculateDiv: " + calculateDiv(number1, number2));
+        System.out.println("checkSomeNumberGreaterThan100: " + checkSomeNumberGreaterThan100(number1, number2));
+        System.out.println("checkWordContainsFancy: " + checkWordContainsFancy(word));
+        System.out.println("checkLengthWordGreaterThan20Chars: " + checkLengthWordGreaterThan20Chars(word));
+
+    }
+
+    // methods
+
+    public static double calculateAdd(int val1, double val2){
+
+        return val1 + val2;
+
+    }
+
+    public static double calculateSub(int val1, double val2){
+
+        return val1 - val2;
+
+    }
+
+    public static double calculateMul(int val1, double val2){
+
+        return val1 * val2;
+
+    }
+
+    public static double calculateDiv(int val1, double val2){
+
+        return val1 / val2;
+
+    }
+
+    public static boolean checkSomeNumberGreaterThan100(int val1, double val2){
+
+        return (val1 > 100 || val2 > 100);
+
+    }
+
+    public static boolean checkWordContainsFancy(String word1){
+
+        return word1.contains("fancy");
+
+    }
+
+    public static boolean checkLengthWordGreaterThan20Chars(String word1){
+
+        return (word1.length() > 20);
+
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,186 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testCalcAdd(){
+
+        //GIVE
+
+        int a = 2;
+        double b = 3;
+
+        //WHEN
+
+        double result = Calculator.calculateAdd(a, b);
+
+        //THEN
+
+        Assertions.assertEquals(5, result);
+
+    }
+
+    @Test
+    void testCalcSub(){
+
+        //GIVE
+
+        int a = 2;
+        double b = 3;
+
+        //WHEN
+
+        double result = Calculator.calculateSub(a, b);
+
+        //THEN
+
+        Assertions.assertEquals(-1, result);
+
+    }
+
+    @Test
+    void testCalcMul(){
+
+        //GIVE
+
+        int a = 2;
+        double b = 3;
+
+        //WHEN
+
+        double result = Calculator.calculateMul(a, b);
+
+        //THEN
+
+            Assertions.assertEquals(6, result);
+
+    }
+
+    @Test
+    void testCalcDiv(){
+
+        //GIVE
+
+        int a = 2;
+        double b = 4;
+
+        //WHEN
+
+        double result = Calculator.calculateDiv(a, b);
+
+        //THEN
+
+        Assertions.assertEquals(0.5, result);
+
+    }
+
+    @Test
+    void testCheckGreaterThan100True(){
+
+        //GIVE
+
+        int a = 200;
+        double b = 4;
+
+        //WHEN
+
+        boolean result = Calculator.checkSomeNumberGreaterThan100(a, b);
+
+        //THEN
+
+        Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    void testCheckGreaterThan100False(){
+
+        //GIVE
+
+        int a = 5;
+        double b = 4;
+
+        //WHEN
+
+        boolean result = Calculator.checkSomeNumberGreaterThan100(a, b);
+
+        //THEN
+
+        Assertions.assertFalse(result);
+
+    }
+
+    @Test
+    void testCheckFancyTrue(){
+
+        //GIVE
+
+        String word = "asdfancyasd";
+
+        //WHEN
+
+        boolean result = Calculator.checkWordContainsFancy(word);
+
+        //THEN
+
+        Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    void testCheckFancyFalse(){
+
+        //GIVE
+
+        String word = "asdasd";
+
+        //WHEN
+
+        boolean result = Calculator.checkWordContainsFancy(word);
+
+        //THEN
+
+        Assertions.assertFalse(result);
+
+    }
+
+    @Test
+    void testCheckLengthGreaterThan20CharsTrue(){
+
+        //GIVE
+
+        String word = "asdasasdag4gneklbmemb54b5zt45d";
+
+        //WHEN
+
+        boolean result = Calculator.checkLengthWordGreaterThan20Chars(word);
+
+        //THEN
+
+        Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    void testCheckLengthGreaterThan20CharsFalse(){
+
+        //GIVE
+
+        String word = "asdasat45d";
+
+        //WHEN
+
+        boolean result = Calculator.checkLengthWordGreaterThan20Chars(word);
+
+        //THEN
+
+        Assertions.assertFalse(result);
+
+    }
+
+
+
+
+
 }
